f403a381c1861cf73b7b1da44b6adc43c8b7915f
iai <deadlyparkourkiller228336@gmail.com>
2025-06-05 19:08:32 +0300

    lastfixwithkms

--- src/pages/kms.jsx
+++ src/pages/kms.jsx
@@ -8,6 +8,162 @@ function Kms() {
             <p>На 12.04.2023 года переведено более 200 пользователей</p>
             <p>На первом этапе работ было подключено 80 пользователей включая 30 первоочередных. На втором этапе планировалось подключить к новой высокоскоростной оптоволоконной линии связи (на скорости 100 Мб/с) 170 пользователей (сейчас скорости обмена данными не превышает 2 МБ/с).</p>
             <p>Ввод в строй новой оптоволоконной линии связи позволил увеличить скорость обмена информацией и доступ пользователей «ПК Промтрактор» к данным примерно в 100 раз (было 2 mb/s стало 100 mb/s)</p>
+
+            <h3>КМС</h3>
+            <table className="kms__table--placement">
+                <tr className="table__header--font">
+                    <td>Название</td>
+                    <td className="table__element--placement">Всего</td>
+                    <td className="table__element--placement">Установлено</td>
+                    <td className="table__element--placement">Выполнено</td>
+                </tr>
+                <tr>
+                    <td>Коммутационный шкафы</td>
+                    <td className="table__element--placement">17</td>
+                    <td className="table__element--placement">17</td>
+                    <td className="table__element--placement">100%</td>
+                </tr>
+                <tr>
+                    <td>Проложено оптического кабеля к</td>
+                    <td className="table__element--placement">17</td>
+                    <td className="table__element--placement">17</td>
+                    <td className="table__element--placement">100%</td>
+                </tr>
+                <tr>
+                    <td>Витая пара проложено</td>
+                    <td className="table__element--placement">14</td>
+                    <td className="table__element--placement">14</td>
+                    <td className="table__element--placement">100%</td>
+                </tr>
+                <tr>
+                    <td>Сварено волокон</td>
+                    <td className="table__element--placement">38</td>
+                    <td className="table__element--placement">38</td>
+                    <td className="table__element--placement">100%</td>
+                </tr>
+                <tr>
+                    <td>Установлено розеток</td>
+                    <td className="table__element--placement">14</td>
+                    <td className="table__element--placement">14</td>
+                    <td className="table__element--placement">100%</td>
+                </tr>
+                <tr>
+                    <td>Электропитание подведено к</td>
+                    <td className="table__element--placement">17</td>
+                    <td className="table__element--placement">16</td>
+                    <td className="table__element--placement">94,1%</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td className="table__element--placement"></td>
+                    <td className="table__element--placement"></td>
+                    <td className="table__element--placement">99,0%</td>
+                </tr>
+            </table>
+
+            <h3>ПССК</h3>
+            <table className="kms__table--placement">
+                <tr className="table__header--font">
+                    <td>Название</td>
+                    <td className="table__element--placement">Всего</td>
+                    <td className="table__element--placement">Установлено</td>
+                    <td className="table__element--placement">Выполнено</td>
+                </tr>
+                <tr>
+                    <td>Коммутационный шкафы</td>
+                    <td className="table__element--placement">21</td>
+                    <td className="table__element--placement">20</td>
+                    <td className="table__element--placement">95,2%</td>
+                </tr>
+                <tr>
+                    <td>Проложено оптического кабеля к</td>
+                    <td className="table__element--placement">21</td>
+                    <td className="table__element--placement">21</td>
+                    <td className="table__element--placement">100%</td>
+                </tr>
+                <tr>
+                    <td>Витая пара проложено</td>
+                    <td className="table__element--placement">17</td>
+                    <td className="table__element--placement">17</td>
+                    <td className="table__element--placement">100%</td>
+                </tr>
+                <tr>
+                    <td>Сварено волокон</td>
+                    <td className="table__element--placement">16</td>
+                    <td className="table__element--placement">16</td>
+                    <td className="table__element--placement">100%</td>
+                </tr>
+                <tr>
+                    <td>Установлено розеток</td>
+                    <td className="table__element--placement">17</td>
+                    <td className="table__element--placement">17</td>
+                    <td className="table__element--placement">100%</td>
+                </tr>
+                <tr>
+                    <td>Электропитание подведено к</td>
+                    <td className="table__element--placement">17</td>
+                    <td className="table__element--placement">17</td>
+                    <td className="table__element--placement">100,0%</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td className="table__element--placement"></td>
+                    <td className="table__element--placement"></td>
+                    <td className="table__element--placement">99,2%</td>
+                </tr>
+            </table>
+
+            <h3>КОТ</h3>
+            <table className="kms__table--placement">
+                <tr className="table__header--font">
+                    <td>Название</td>
+                    <td className="table__element--placement">Всего</td>
+                    <td className="table__element--placement">Установлено</td>
+                    <td className="table__element--placement">Выполнено</td>
+                </tr>
+                <tr>
+                    <td>Коммутационный шкафы</td>
+                    <td className="table__element--placement">6</td>
+                    <td className="table__element--placement">6</td>
+                    <td className="table__element--placement">100,00%</td>
+                </tr>
+                <tr>
+                    <td>Проложено оптического кабеля к</td>
+                    <td className="table__element--placement">6</td>
+                    <td className="table__element--placement">6</td>
+                    <td className="table__element--placement">100,00%</td>
+                </tr>
+                <tr>
+                    <td>Витая пара проложено</td>
+                    <td className="table__element--placement">15</td>
+                    <td className="table__element--placement">15</td>
+                    <td className="table__element--placement">100,00%</td>
+                </tr>
+                <tr>
+                    <td>Сварено волокон</td>
+                    <td className="table__element--placement">48</td>
+                    <td className="table__element--placement">48</td>
+                    <td className="table__element--placement">100,00%</td>
+                </tr>
+                <tr>
+                    <td>Установлено розеток</td>
+                    <td className="table__element--placement">6</td>
+                    <td className="table__element--placement">6</td>
+                    <td className="table__element--placement">100,00%</td>
+                </tr>
+                <tr>
+                    <td>Электропитание подведено к</td>
+                    <td className="table__element--placement">6</td>
+                    <td className="table__element--placement">6</td>
+                    <td className="table__element--placement">100,00%</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td className="table__element--placement"></td>
+                    <td className="table__element--placement"></td>
+                    <td className="table__element--placement">100,00%</td>
+                </tr>
+            </table>
         </div>
     )
 }
